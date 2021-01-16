a9c6c2f0e7bb18e7e849981174dd3a5811f020f1
motoyama1020 <babywalk18@gmail.com>
2021-01-16 19:19:08 +0900

    一覧画面の表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_011855) do
+ActiveRecord::Schema.define(version: 2021_01_16_100513) do
+
+  create_table "ramen_shops", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
